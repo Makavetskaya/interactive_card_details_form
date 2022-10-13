002fafdc27e7dd023cabbce255332d74977d506d
Makavetskaya Tatsiana <makavetskaya.t@gmail.com>
2022-10-13 17:06:13 +0300

    add function btnSubmit

--- src/app/form2.js
+++ src/app/form2.js
@@ -1,9 +1,12 @@
 const form = document.getElementById('form')
+const modal = document.getElementById('modal')
 const nameCard = document.getElementById('nameCard')
 const numberCard = document.getElementById('numberCard')
 const monthCard = document.getElementById('monthCard')
 const yearCard = document.getElementById('yearCard')
 const cvcCard = document.getElementById('cvcCard')
+const btn = document.getElementById('submit')
+const modalClose = document.querySelector('.close')
 
 
 ///// errors 
@@ -14,11 +17,60 @@ const errorCvc = document.querySelector('.error-cvc')
 
 
 
-form.addEventListener('submit', e =>{
-    e.preventDefault()
-    validationInputs()
+
+form.addEventListener('input', validationInputs)
+// btn.addEventListener('submit', btnSubmit)
+btn.addEventListener('click', (e) =>{
+    let isAllValid = []
+    arrInputIsValid.forEach(el =>{
+        isAllValid.push(el.getAttribute('is-valid'))
+    })
+    console.log(isAllValid)
+
+    let isValid = isAllValid.reduce((acc, curr) =>{
+        return acc && curr
+    })
+    console.log(isValid)
+    if(isValid === "0"){
+        e.preventDefault()
+        btn.classList.add('animation')
+    } else{
+        modal.style.display = 'block'
+    }
+
+    
+    })
+
+
+let arrInput = Array.from(form)
+console.log(arrInput)
+
+let arrInputIsValid = []
+
+arrInput.forEach( el =>{
+    if(el.hasAttribute('data-reg')){
+        el.setAttribute('is-valid', '0')
+        arrInputIsValid.push(el)
+    }
 })
 
+console.log(arrInputIsValid)
+
+
+// function btnSubmit(valid){
+//     let isAllValid = []
+//     arrInputIsValid.forEach(el =>{
+//         isAllValid.push(el.getAttribute('is-valid'))
+//     })
+//     console.log(isAllValid)
+
+//     let isValid = isAllValid.reduce((acc, curr) =>{
+//         return acc && curr
+//     })
+//     console.log(isValid)
+    
+    
+// }
 function validationInputs(){
     const nameCardValue = nameCard.value.trim()
     const numberCardValue = numberCard.value.trim()
@@ -29,45 +81,62 @@ function validationInputs(){
         ///// name
         if (nameCardValue === ''){
             notInvalid(nameCard, errorName, "Can't be blank")
+            nameCard.setAttribute('is-valid', '0')
         }else if(!nameCardValue.match(/^[a-zA-Z]+\s[A-Za-z]*$/g)){
             notInvalid(nameCard, errorName, 'Wrong format, letters only')
+            nameCard.setAttribute('is-valid', '0')
         }else{
-            invalid(nameCard)
+            invalid(nameCard, errorName, '')
+            nameCard.setAttribute('is-valid', '1')
+
         }
          ////// number
         if (numberCardValue === ''){
             notInvalid(numberCard, errorNumber, "Can't be blank")
-        }else if(!regNumberCard(numberCardValue)){
+            numberCard.setAttribute('is-valid', '0')
+        }else if(numberCardValue.length < 16){
             notInvalid(numberCard, errorNumber, "Not a valid number")
+            numberCard.setAttribute('is-valid', 'false')
         }else{
-            invalid(numberCard)
+            invalid(numberCard, errorNumber, '')
+            numberCard.setAttribute('is-valid', '1')
         }
         ///// month
         if(monthCardValue === ''){
             notInvalid(monthCard, errorData,  "Can't be blank")
+            monthCard.setAttribute('is-valid', '0')
         } else if(!regMomthCard(monthCardValue)){
             notInvalid(monthCard, errorData,  "Not a valid data")
+            monthCard.setAttribute('is-valid', '0')
         }else{
-            invalid(monthCard)
+            invalid(monthCard, errorData, '')
+            monthCard.setAttribute('is-valid', '1')
         }
 
         ///// year
         if(yearCardValue === ''){
             notInvalid(yearCard, errorData, "Can't be blank" )
+            yearCard.setAttribute('is-valid', '0')
         }else if(!regYearCard(yearCardValue)){
             notInvalid(yearCard, errorData, "Not a valid data" )
+            yearCard.setAttribute('is-valid', '0')
         } else{
-            invalid(yearCard)
+            invalid(yearCard,errorData, '')
+            yearCard.setAttribute('is-valid', '1')
         }
        
         ///// cvc 
         if(cvcCardValue === ''){
             notInvalid(cvcCard, errorCvc, "Can't be blank" )
+            cvcCard.setAttribute('is-valid', '0')
         } else if(!regCvcCard(cvcCardValue)){
             notInvalid(cvcCard, errorCvc, "Not a valid cvc" )
+            cvcCard.setAttribute('is-valid', '0')
         } else{
-            invalid(cvcCard)
+            invalid(cvcCard, errorCvc, '')
+            cvcCard.setAttribute('is-valid', '1')
         }
+      
    
    
    
@@ -79,8 +148,10 @@ function notInvalid(input,el,message){
     
 }
 
-function invalid (input){
+function invalid (input,el,message){
     input.classList.remove('error-invalid')
+    el.textContent= message
+   
   
    
 }
@@ -92,7 +163,7 @@ function regNameCard(name){
 }
 
 function regNumberCard(number){
-     return /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$/.test(number)
+     return /^[0-9]{3}$/.test(number)
 }
 
 function regMomthCard(month){
@@ -103,7 +174,14 @@ function regYearCard(year){
     return /^(19|20)[\d]{2,2}$/.test(year)
 }
 function regCvcCard(cvc){
-    return /^[0-9]{3, 4}$/.test(cvc)
+    return /^[0-9]{3}$/.test(cvc)
 }
+
+
+modalClose.addEventListener('click',()=>{
+   modal.style.display = 'none',
+   form.style.display = 'block'
+})
+
 
 
--- src/app/handlers.js
+++ src/app/handlers.js
@@ -3,26 +3,26 @@
 
 const nameInput = document.querySelector('.name-input')
 nameInput.addEventListener('input', e =>{
-    document.querySelector('.name').textContent = e.target.value
+    document.querySelector('.name').innerHTML = e.target.value
 })
 
 const numberInput = document.querySelector('.number-input')
 numberInput.addEventListener('input', e=>{
-    document.querySelector('.output-number').textContent = e.target.value
+    document.querySelector('.output-number').innerHTML = e.target.value
 })
 
 const monthInput = document.querySelector('.month-input')
 monthInput.addEventListener('input',e=>{
-    document.querySelector('.month-output').textContent = e.target.value
+    document.querySelector('.month-output').innerHTML = e.target.value
 })
 
 const yearInput = document.querySelector('.year-input')
 yearInput.addEventListener('input',e=>{
-    document.querySelector('.year-output').textContent = e.target.value
+    document.querySelector('.year-output').innerHTML= e.target.value
 })
 
 const cvInput = document.querySelector('.cv-input')
 cvInput.addEventListener('input',e=>{
-    document.querySelector('.output-cv').textContent = e.target.value
+    document.querySelector('.output-cv').innerHTML = e.target.value
 })
 
